b0d35607c69d6cd05c200e081b02324deaa6f411
seesawin <seesawin@gmail.com>
2018-08-06 17:46:26 +0800

    2018/08/06 update

--- src/main/java/com/deppon/text01/action/UserServlet.java
+++ src/main/java/com/deppon/text01/action/UserServlet.java
@@ -12,28 +12,27 @@ import javax.servlet.http.HttpServletResponse;
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=utf-8");
 
 		printHeader(request);
-		
+
 		String action = request.getParameter("action");
 
 		if ("login_input".equals(action)) {
-			request.getRequestDispatcher("login.jsp")
-					.forward(request, response);
+			request.getRequestDispatcher("login.jsp").forward(request, response);
 		} else if ("login".equals(action)) {
 			String name = request.getParameter("name");
 			String password = request.getParameter("password");
 
 			System.out.println("name->" + name + ",password->" + password);
+			
+			request.getRequestDispatcher("show.jsp").forward(request, response);
 		}
 	}
 
@@ -48,38 +47,38 @@ public class UserServlet extends HttpServlet {
 				sb.append("<" + header + "> : " + request.getHeader(header) + "\n");
 			}
 		}
-		
+
 		System.out.println("請求標頭資訊: ");
 		System.out.println(sb.toString());
-		
+
 		System.out.println("取得用戶端資訊: ");
 		System.out.println("請求的伺服器: " + request.getServerName());
 		System.out.println("使用協定： " + request.getProtocol());
 		System.out.println("請求方法： " + request.getMethod());
 		System.out.println("請求的埠號： " + request.getServerPort());
-		System.out.println("Context路徑： " + request.getContextPath() );
+		System.out.println("Context路徑： " + request.getContextPath());
 		System.out.println("Servlet路徑： " + request.getServletPath());
 		System.out.println("URI路徑： " + request.getRequestURI());
 		System.out.println("查詢字串： " + request.getQueryString());
-	 
+
 		System.out.println("使用者主機IP： " + request.getRemoteAddr());
 		System.out.println("使用者使用埠號： " + request.getRemotePort());
-		
+
 		// POST資料
 		StringBuilder sBuilder = new StringBuilder();
 		BufferedReader buffer = request.getReader();
-		
+
 		String line = "";
-		while((line = buffer.readLine()) != null) {
+		while ((line = buffer.readLine()) != null) {
 			sBuilder.append(line);
 		}
-		
+
 		System.out.println("message-body : " + sBuilder.toString());
-		
+
 		// GET資料
 		String qStr = request.getQueryString();
 		System.out.println("queryString : " + qStr);
-		
+
 	}
 
 }
